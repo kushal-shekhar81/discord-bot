2cb6097144c3a027d4b0a0d039a77e5ba7cc9d9c
Kushal Shekhar Pradhan <kushal.shekhar@hotmail.com>
2020-10-04 00:49:19 +0530

    Hello

---
+++ Bot.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext.commands import Bot
+from random import choice
+
+bot = Bot("!")
+
+
+with open('compliments.txt','r') as file:
+    complimentList = list(file)
+
+with open('jokes.txt','r') as joke:
+    jokesList = list(joke)
+
+def getCompliment():
+    return choice(complimentList)
+
+def getJoke():
+    return choice(jokesList)
+
+@bot.event
+async def on_ready():
+    print('Logged in')
+
+@bot.event
+async def on_message(message):
+    print(message.author,message.content,message.channel)
+    
+    if message.content[0] != "!":
+        if message.author != bot.user:
+            await message.channel.send(str(message.content) + " " + str(message.author.mention) )
+    
+    
+    await bot.process_commands(message)
+    
+
+@bot.command(pass_context=True)
+async def compliment(ctx, member:discord.Member):
+    membersList = ctx.message.guild.members
+    print(membersList)
+    complimentToSend = getCompliment()
+    complimentToSend += member.mention
+    await ctx.send(complimentToSend)
+
+@bot.command(pass_context=True)
+async def joke(ctx, member:discord.Member):
+    jokeToSend = getJoke()
+    jokeToSend += member.mention
+    await ctx.send(jokeToSend)
+
+bot.run('') 
+
+#Replace your bot token inside the (''), and run the program
---
+++ README.md
@@ -0,0 +1,10 @@
+This is the code for a simple Discord Bot, which echoes and compliments users.
+Run "pip install requirements.txt" before starting with the projects.
+
+How to use?
+1. Go to https://discord.com/developers/ , and create a new application there.
+2. Enter the name for your bot, and select "Create"
+3. Choose "Bot" under Settings, and choose 'Click to reveal token' (or Regenerate if you are working for second or consecutive time). Copy the token generated, and paste it in the Bot.py program at the marked location
+4. Once the python program has been saved and compiled, back in the discord developers website select on "OAuth2" under settings; Under 'Scopes' choose 'Bot', and copy the url (which will appear below) and paste it in a new browser to connect the bot to your server.
+
+Once the bot is connected, run the Bot.py program, and once it is running go to the Discord server and try! 
